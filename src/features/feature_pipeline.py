6eb1bb10a13cec9733ec62f5bc1408407855d16a
bekam <bekamgenene@gmail.com>
2025-12-14 21:37:47 +0300

    all fixed issue

--- src/features/feature_pipeline.py
+++ src/features/feature_pipeline.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
-from typing import List
 
-import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.compose import ColumnTransformer
